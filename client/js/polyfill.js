8fed23dff1ad80dc3862ec8e8135c01ce7111b45
Carlos Moreira <carlos.moreira@infobase.com.br>
2018-12-20 20:35:10 -0200

    Adds new polyfill to array

--- client/js/polyfill.js
+++ client/js/polyfill.js
@@ -1,7 +1,15 @@
-(function() {
-    if (!Array.isArray) {
-        Array.isArray = function(arg) {
-          return Object.prototype.toString.call(arg) === '[object Array]';
-        };
-      }
-})();
+if (!Array.isArray) {
+  console.log('Polyfill to Array.isArray aplied.');
+    Array.isArray = function(arg) {
+      return Object.prototype.toString.call(arg) === '[object Array]';
+    };
+  }
+
+if(!Array.prototype.includes) {
+
+  console.log('Polyfill to Array.includes aplied.');
+
+  Array.prototype.includes = function(elemento) {
+      return this.indexOf(elemento) != -1;
+  };
+}
