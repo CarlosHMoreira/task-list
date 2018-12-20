e0d9974ca2d31251673be97575949ea24d9f2538
Carlos Moreira <carlos.moreira@infobase.com.br>
2018-12-20 20:19:49 -0200

    Changes name of file TaskList to TaskListView

---
+++ client/js/app/views/TaskListView.js
@@ -0,0 +1,33 @@
+class TaskListView extends View {
+
+    template(model) {
+        return `
+        <table class="table table-hover table-bordered task-list">
+            <thead>
+                <tr class="task-list__headers-row">
+                    <th onclick="taskController.orderColumn('date')">Data</th>
+                    <th onclick="taskController.orderColumn('name')">Nome</th>
+                    <th onclick="taskController.orderColumn('priority')">Prioridade</th>
+                    <th onclick="taskController.orderColumn('done')">Feita</th>
+                </tr>
+            </thead>
+            
+            <tbody>
+                ${model.tasks.map(task => `
+                    <tr>
+                        <td>${DateHelper.dateToString(task.date)}</td>
+                        <td>${task.name}</td>
+                        <td>${PriorityEnum[task.priority]}</td>
+                        <td>${task.done ? `Sim` : '<span class="text-danger">Não<span>'}</td>
+                    </tr>
+                `).join('')}
+            </tbody>
+            
+            <tfoot>
+                <td colspan="3"></td>
+                <td>${model.totalFinished()}</td>
+            </tfoot>
+        </table>
+        `;
+    }
+}
